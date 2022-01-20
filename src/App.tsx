9ef2ee9403b4be4cf106f32898ef7cdc82eb6e81
Thomas O'Neill <tom@opensystemslab.io>
2022-01-20 18:04:02 +0000

    TS2322 repro

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { pipe } from "fp-ts/lib/function"
+import { collect } from "fp-ts/lib/ReadonlyRecord"
+import { Ord } from "fp-ts/lib/string"
+import React, { Fragment } from "react"
+import { proxy, useSnapshot } from "valtio"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type HouseT = {
+  id: string
+  position: [number, number]
 }
 
-export default App;
+const store = proxy<{
+  houses: {
+    [key: string]: HouseT
+  }
+}>({
+  houses: {
+    foo: {
+      id: "foo",
+      position: [0, 0],
+    },
+  },
+})
+
+type HouseViewProps = {
+  house: Readonly<HouseT>
+}
+
+const HouseView = (props: HouseViewProps) => {
+  return (
+    <div>
+      <pre>{JSON.stringify(props, null, 2)}</pre>
+    </div>
+  )
+}
+
+function App() {
+  const { houses } = useSnapshot(store)
+
+  return (
+    <Fragment>
+      {pipe(
+        houses,
+        collect(Ord)((id, house) => (
+          <div key={id}>
+            <HouseView key={house.id} house={house} />
+          </div>
+        ))
+      )}
+    </Fragment>
+  )
+}
+
+export default App
